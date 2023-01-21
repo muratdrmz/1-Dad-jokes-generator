b76343776c220eb9cbc653c8a0c2d35f4ba97576
muratdrmz <mrtdrmz@windowslive.com>
2023-01-21 12:34:48 +0100

    End

--- script.js
+++ script.js
@@ -13,17 +13,25 @@ const options={
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 
 async function getJoke(){
- jokeEl.innerHTML='Updating...'
- btnEl.disable=true;
- btnEl.innerHTML='Loading...';
-const response= await fetch(apiURL,options)
-const data= await response.json();
-// console.log(data[0].joke);
-// console.log(data);
-btnEl.disable=false;
-btnEl.innerHTML='Tell me more joke';
 
-jokeEl.innerHTML=data[0].joke;
+ try {
+  jokeEl.innerHTML='Updating...'
+  btnEl.disable=true;
+  btnEl.innerHTML='Loading...';
+ const response=await fetch(apiURL,options)
+ const data= await response.json();
+ // console.log(data[0].joke);
+ // console.log(data);
+ btnEl.disable=false;
+ btnEl.innerHTML='Tell me more joke';
+ 
+ jokeEl.innerHTML=data[0].joke;  
+ } catch (error) {
+  jokeEl.innerHTML=`Following error happend: ${error}. Try again later`
+  btnEl.disable=false;
+  btnEl.innerHTML='Tell me a joke'
+  console.log(error);  
+ }
 }
 
 
